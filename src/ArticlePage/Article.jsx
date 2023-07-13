8246379df4da6f5bc0fa5889bae6b662901e81ed
Najaf Ali <najaf.balti000@gmail.com>
2023-07-13 21:43:12 +0500

    3rd Push

--- src/ArticlePage/Article.jsx
+++ src/ArticlePage/Article.jsx
@@ -6,7 +6,16 @@ const Article = () => {
   return (
     <div>
       <ArticleHero />
-      <ArticleComponent />
+      <ArticleComponent
+      catIndex="1"
+        mainHeading="Marrobbio, Sliema & St Julian's"
+        imgSource="https://maltaa.simationtechnologies.com/wp-content/uploads/2023/02/Delicious-Food-2.jpg"
+        firstPara="This unapologetically Italian eatery is not only home to one of my
+          favourite pizzas in Malta, but also to a carbonara that will please
+          all purists. On the plate, it doesn’t look too appetising, however on
+          the palate… it’s the real deal!"
+        secondPara="Price: 10EUR as a starter and 12EUR as main"
+      />
     </div>
   );
 };
